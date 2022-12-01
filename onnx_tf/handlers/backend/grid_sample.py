fd1407d1fc73da6952cacb49ee5c9f0df2bcf10e
HimariO <dsfhe49854@gmail.com>
2022-12-01 12:30:58 +0800

    Always apply transpose to NHWC before GridSample

--- onnx_tf/handlers/backend/grid_sample.py
+++ onnx_tf/handlers/backend/grid_sample.py
@@ -17,23 +17,18 @@ class GridSample(BackendHandler):
     x = input_dict[node.inputs[0]]
     grid = input_dict[node.inputs[1]]
     
-    print('-' * 100)
-    print(type(x))
-    print(x)
-    print(grid)
-    # print(shape_4d)
-    print('-' * 100)
-
-    # HACK: not sure how to determint input data format is been transpose outside of node or not
-    c_last = 'transpose' in x.name.lower()
-    if not c_last:
-      x = tf.transpose(x, perm=[0, 2, 3, 1])
+    x = tf.transpose(x, perm=[0, 2, 3, 1])
     shape_4d = tf.shape(grid)
     grid_3d = tf.reshape(grid, [shape_4d[0], shape_4d[1] * shape_4d[2], shape_4d[3]])
     
     y = tfa.image.interpolate_bilinear(x, grid_3d)
     y = tf.reshape(y, [shape_4d[0], shape_4d[1], shape_4d[2], -1])
-    if not c_last:
-      y = tf.transpose(y, perm=[0, 3, 1, 2])
+    y = tf.transpose(y, perm=[0, 3, 1, 2])
     
+    # print('-' * 100)
+    # print(x)
+    # print(grid)
+    # print(y)
+    # # print(shape_4d)
+    # print('-' * 100)
     return [y]
